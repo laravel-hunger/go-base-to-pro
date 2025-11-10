7f5348de52dd9e6bdbe25e9b050953df29ac0976
tanvir.bappy <tanvir.bappy@bansberrysg.com>
2025-11-10 15:29:06 +0600

    values

---
+++ values.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello" + " Golang")
+	fmt.Println("1+1: ", 1+1)
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
