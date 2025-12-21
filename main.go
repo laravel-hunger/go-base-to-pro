f7a0894c489e29e85021081b30fb6bc8e45df9cc
tanvir.bappy <tanvir.bappy@bansberrysg.com>
2025-12-21 16:44:32 +0600

    --

--- main.go
+++ main.go
@@ -1,16 +1,33 @@
 package main
 
-import "fmt"
-
-func higherFunc(x, y int, amarvai func(a, b int)) {
-	amarvai(x, y)
-}
-
-func emon(a, b int) {
-	c := a + b
-	fmt.Println(c)
-}
+import (
+	"fmt"
+	"sort"
+)
 
 func main() {
-	higherFunc(10, 20, emon)
+	fmt.Println("Hello Slices")
+	var fruitList = []string{"Apple", "Banana", "Orange"}
+	fmt.Printf("Type of fruitList is  %T\n", fruitList)
+
+	fruitList = append(fruitList, "Mango", "Grapes")
+	fmt.Println(fruitList)
+	fruitList = append(fruitList[1:3])
+	fmt.Println(fruitList) // ["Banana", "Orange"]
+
+	highScores := make([]int, 5)
+
+	highScores[0] = 865
+	highScores[1] = 645
+	highScores[2] = 756
+	highScores[3] = 800
+	highScores[4] = 532
+	highScores = append(highScores, 782)
+
+	fmt.Println(highScores, len(highScores), cap(highScores))
+
+	sort.Ints(highScores)
+
+	fmt.Println(highScores, sort.IntsAreSorted(highScores))
+
 }
